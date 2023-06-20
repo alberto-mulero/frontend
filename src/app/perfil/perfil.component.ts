91eb713f2252e58f15d458c6d975db66db3b43dd
JaviVele <javivelez42@gmail.com>
2023-06-20 17:01:06 +0200

    popup v8

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -111,7 +111,7 @@ export class PerfilComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogComponent, {
       width: "50%",
       height: "72%",
-      position: {top: "-100%", left: "25%" },
+      position: {bottom: "100%", left: "25%" },
       data: { "usuario": this.usuario }
     });
 
